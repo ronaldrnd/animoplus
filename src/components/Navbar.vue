c1e745f36ba38a04fc7ad9d6eb78a9d83832ddf9
ezra.fa <ezra.fa@centraltest.com>
2025-06-10 16:57:14 +0200

    configuration css and structure

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,32 @@
 <template>
-    Navbar
+    <div class="bg-primary-600 w-full flex justify-between items-center py-[48px] px-[44px]">
+        <img class="w-28 h-9" src="../assets/logo_header.png" />
+        <div class="flex justify-start items-center gap-11">
+            <div class="flex justify-start items-center gap-11">
+                <div class="justify-start text-white text-lg font-medium">Téléconseil</div>
+                <div class="justify-start text-white text-lg font-medium">Vous êtes un pro
+                </div>
+            </div>
+            <div class="flex justify-start items-center gap-6">
+                <div
+                    class="px-6 py-3 bg-green-600 rounded-xl shadow-[0px_1px_3px_0px_rgba(0,0,0,0.05)] outline outline-1 outline-offset-[-1px] outline-gray-200 flex justify-center items-center gap-2.5">
+                    <div class="justify-start text-white text-lg">S’inscrire
+                    </div>
+                </div>
+                <div
+                    class="px-6 py-3 bg-white rounded-xl shadow-[0px_1px_3px_0px_rgba(0,0,0,0.05)] outline outline-1 outline-offset-[-1px] outline-gray-200 flex justify-center items-center gap-2.5">
+                    <div class="justify-start text-green-600 text-lg">Se
+                        connecter</div>
+                    <div class="w-4 h-4 relative overflow-hidden">
+                        <div
+                            class="w-1 h-3 left-[10px] top-[2px] absolute outline outline-[1.33px] outline-offset-[-0.67px] outline-emerald-600" />
+                        <div
+                            class="w-[3.33px] h-1.5 left-[6.67px] top-[4.67px] absolute outline outline-[1.33px] outline-offset-[-0.67px] outline-emerald-600" />
+                        <div
+                            class="w-2 h-0 left-[2px] top-[8px] absolute outline outline-[1.33px] outline-offset-[-0.67px] outline-emerald-600" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
